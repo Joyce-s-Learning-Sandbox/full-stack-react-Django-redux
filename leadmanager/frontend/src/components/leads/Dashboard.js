f694290c4756fc237b66ea142bfdf721ece3d733
jiayu-joyce-he <jiayuhe@alumni.stanford.edu>
2020-02-04 08:42:26 -0800

    bilerplate finish

---
+++ leadmanager/frontend/src/components/leads/Dashboard.js
@@ -0,0 +1,14 @@
+/** @format */
+
+import React from 'react'
+import Form from './Form'
+import Leads from './Leads'
+
+export default function Dashboard() {
+    return (
+        <>
+            <Form />
+            <Leads />
+        </>
+    )
+}
---
+++ leadmanager/frontend/src/components/leads/Leads.js
@@ -0,0 +1,9 @@
+/** @format */
+
+import React, { Component } from 'react'
+
+export default class Leads extends Component {
+    render() {
+        return <h1>Leads List</h1>
+    }
+}
