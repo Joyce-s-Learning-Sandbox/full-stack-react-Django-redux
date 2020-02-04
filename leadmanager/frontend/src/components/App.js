8e93e33bfb779d9ab17fd965d6e6f5a4de2076cd
jiayu-joyce-he <jiayuhe@alumni.stanford.edu>
2020-02-04 08:28:47 -0800

    configure

---
+++ leadmanager/frontend/src/components/App.js
@@ -0,0 +1,12 @@
+/** @format */
+
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+
+class App extends Component {
+    render() {
+        return <div>React Here</div>
+    }
+}
+
+ReactDOM.render(<App />, document.getElementById('app'))
--- leadmanager/leadmanager/urls.py
+++ leadmanager/leadmanager/urls.py
@@ -17,5 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('', include('frontend.urls')),   # make sure the frontend loads first
     path('', include('leads.urls')),
 ]
