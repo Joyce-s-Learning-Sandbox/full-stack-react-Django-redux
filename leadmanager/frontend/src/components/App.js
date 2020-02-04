d071987007337511225d4a3fffa10bb01f69de5e
jiayu-joyce-he <jiayuhe@alumni.stanford.edu>
2020-02-04 08:35:52 -0800

    add nav bar

--- leadmanager/frontend/src/components/App.js
+++ leadmanager/frontend/src/components/App.js
@@ -3,9 +3,11 @@
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
 
+import Header from './layout/Header'
+
 class App extends Component {
     render() {
-        return <div>React Here</div>
+        return <Header />
     }
 }
 
