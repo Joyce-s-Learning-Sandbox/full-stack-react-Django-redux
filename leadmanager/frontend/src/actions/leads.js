00f8d8452cd2332df0b82d13177f1e59cf3a4b0f
jiayu-joyce-he <jiayuhe@alumni.stanford.edu>
2020-02-07 09:12:39 -0800

    add add and delete alerts; todo: fix adding and error message check

--- leadmanager/frontend/src/actions/leads.js
+++ leadmanager/frontend/src/actions/leads.js
@@ -1,7 +1,8 @@
 /** @format */
 
 import axios from 'axios'
-import { GET_LEADS, DELETE_LEAD, ADD_LEAD } from './types' // import the type
+import { createMessage } from './messages'
+import { GET_LEADS, DELETE_LEAD, ADD_LEAD, GET_ERRORS } from './types' // import the type
 
 // make HTTP requests
 
@@ -24,6 +25,7 @@ export const deleteLead = id => dispatch => {
     axios
         .delete(`/api/leads/${id}/`)
         .then(res => {
+            dispatch(createMessage({ deleteLead: 'lead deleted' }))
             dispatch({
                 type: DELETE_LEAD,
                 payload: id
@@ -37,10 +39,20 @@ export const addLead = lead => dispatch => {
     axios
         .post('/api/leads/', lead)
         .then(res => {
+            dispatch(createMessage({ addLead: 'Lead Added' }))
             dispatch({
                 type: ADD_LEAD,
                 payload: res.data
             })
         })
-        .catch(err => console.log(err.response.data))
+        .catch(err => {
+            const errors = {
+                msg: err.response.data,
+                status: err.response.status
+            }
+            dispatch({
+                type: GET_ERRORS,
+                payload: errors
+            })
+        })
 }
--- leadmanager/frontend/src/components/layout/Alerts.js
+++ leadmanager/frontend/src/components/layout/Alerts.js
@@ -1,14 +1,46 @@
 /** @format */
 
-import React, { useEffect, Fragment } from 'react'
+import React, { useEffect, useRef, useState, Fragment } from 'react'
 import { withAlert } from 'react-alert'
+import { connect } from 'react-redux'
+import PropTypes from 'prop-types'
 
-const Alerts = ({ alert }) => {
+const Alerts = ({ alert, error, message }) => {
+    const propTypes = {
+        errors: PropTypes.object.isRequired,
+        message: PropTypes.object.isRequired
+    }
+    const prevErrorRef = useRef()
+    const [prevError, setPrevError] = useState({})
+
     useEffect(() => {
-        alert.show('it works')
-    }, [])
+        prevErrorRef.current = error
+        if (error !== prevError) {
+            if (error.msg.name) alert.error(`Name: ${error.msg.name.join()}`)
+            if (error.msg.email) alert.error(`Email: ${error.msg.email.join()}`)
+            if (error.msg.message)
+                alert.error(`Message: ${error.msg.message.join()}`)
+        }
+        return () => {
+            setPrevError(prevErrorRef.current)
+        }
+    }, [error])
+
+    console.log(!(error !== prevError && Object.keys(prevError).length != 0))
+    if (
+        !(error !== prevError && Object.keys(prevError).length != 0) &&
+        message !== prevError.message
+    ) {
+        if (message.deleteLead) alert.success(message.deleteLead)
+        if (message.addLead) alert.success(message.addLead)
+    }
 
     return <Fragment />
 }
 
-export default withAlert()(Alerts)
+const mapStateToProps = state => ({
+    error: state.errors,
+    message: state.messages
+})
+
+export default connect(mapStateToProps)(withAlert()(Alerts))
